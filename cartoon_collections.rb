c546b8a5a4c44241651923be9085ad2a1e5f8aca
Gavin Soko <sokolofg@gmail.com>
2018-08-14 08:24:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ end
 end
 
 def summon_captain_planet(calls)# code an argument here
-  # Your code here
+  calls.map do |el|
+    calls[el] = el.upcase
+    calls [el]# Your code here
 end
 
 def long_planeteer_calls# code an argument here
