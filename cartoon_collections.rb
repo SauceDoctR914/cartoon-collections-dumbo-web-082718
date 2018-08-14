4c39e8f033c8a59880ced23587de1cc0ce323fdb
Gavin Soko <sokolofg@gmail.com>
2018-08-14 08:33:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,6 @@ def long_planeteer_calls(array)
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
  array.map do |item|
-  cheese_types.include(item)
+  cheese_types.include?(item)
 end
 end
